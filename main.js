dbec445be6c5c8df44f3392403482318ca717b4e
Mateus Henrique <nunessuetam@gmail.com>
2025-06-05 17:51:16 -0300

    Corrigindo a pesquisa de times

--- main.js
+++ main.js
@@ -58,119 +58,126 @@ const main = async (time) => {
         await page.click("#search-window")
         await page.waitForSelector(".searchInput__input");
         await page.type(".searchInput__input", time);
-        await page.waitForSelector('.searchResult')
+        await page.waitForSelector('.searchResult>.searchResult__participantName')
 
-        //Get team correct name
-        teamSearch= await page.evaluate(()=>{
-            return time= document.querySelector('.searchResult__participantName').innerText
-        })
-         
+    
+        const {index, correctName} = await page.evaluate((userInput) => {
+            const elements = Array.from(document.querySelectorAll('.searchResult__participantName'));
+            const idx = elements.findIndex((e) => { return e.innerText.trim().toLowerCase() === userInput.trim().toLowerCase() });
+            return { index: idx, correctName: elements[idx].innerText }
+        }, time)
+
 
         await Promise.all([
-            page.waitForNavigation(),
-            page.click('.searchResult')
+            await page.$$eval('.searchResult__participantName', (elements, idx) => {
+                if (elements[idx]) elements[idx].click();
+            }, index),
+        page.waitForNavigation()
+            
 
         ])
 
 
-        // await page.waitForSelector('#li2')
-        // const link = await page.evaluate(() => {
-        //     const link = document.querySelector('#li2').getAttribute('href');
-        //     return link;
-        // })
+// await page.waitForSelector('#li2')
+// const link = await page.evaluate(() => {
+//     const link = document.querySelector('#li2').getAttribute('href');
+//     return link;
+// })
 
 
-        // await page.goto(`https://www.flashscore.com.br${link}`, { timeout: 10000 });
+// await page.goto(`https://www.flashscore.com.br${link}`, { timeout: 10000 });
 
 
-        // Ambiente de teste
+// Ambiente de teste
 
-        await page.waitForSelector('.summary-results')
-        const nextFixture= await page.evaluate(() => {
-            const nextFixture = document.querySelector('.summary-fixtures>.sportName>.event__match');
-            const home = nextFixture.querySelector('.event__homeParticipant').innerText;
-            const away = nextFixture.querySelector('.event__awayParticipant').innerText;
-            return {nextMatch: `${home} vs ${away}`};
-        })
+await page.waitForSelector('.summary-results')
+const nextFixture = await page.evaluate(() => {
+    const nextFixture = document.querySelector('.summary-fixtures>.sportName>.event__match');
+    const home = nextFixture.querySelector('.event__homeParticipant').innerText;
+    const away = nextFixture.querySelector('.event__awayParticipant').innerText;
+    return { nextMatch: `${home} vs ${away}` };
+})
 
-        // fim do ambiente de teste
+// fim do ambiente de teste
 
-        const linkMatchs = await page.evaluate(() => {
-            // before: .event__match>a
-            const matchs = Array.from(document.querySelectorAll('.summary-results>.sportName>.event__match>a'));
-            const link = matchs.map((e) => {
-                return e.getAttribute('href');
-            })
-            return link;
-        })
-
-
-
-        const awaitStatistics = async () => {
-
-            try {
-                await page.waitForSelector('.wcl-category_ITphf', {timeout:10000})
-                await page.waitForSelector('.participant__participantName>a')
-                const cornerKicks = await page.evaluate(() => {
-                    const teams = Array.from(document.querySelectorAll('.participant__participantName>a'));
-                    const wrapper = Array.from(document.querySelectorAll('.wcl-category_ITphf'));
-                    const content = wrapper.map((e) => {
-                        const contenItSelf = Array.from(e.querySelectorAll('strong'));
-
-                        return contenItSelf.map(e => e.innerText)
-                    });
-
-                    // Organize content into an object
-                    const newContent = content.reduce((acc, e) => {
-                        acc[e[1]] = { home: e[0], away: e[2] };
-                        return acc;
-                    }, {})
-
-                
-
-                    return {
-                        Teams: { Teamhome: teams[0].innerText, TeamAway: teams[1].innerText },
-                        Content: newContent,
-                    };
-
-                });
-
-                return cornerKicks
-            } catch (err) {
-                console.error('Erro ao buscar estatísticas: '+ err);
-                return {erro: 'partida sem dados'}
-            }
-
-        }
-
-        matchs['teamSearch']= teamSearch;
-        matchs['nextFixture']= nextFixture.nextMatch;
-        const eachfixture={}
-        for (i = 0; i <= 4; i++) {
-            try {
-                const fullLink = linkMatchs[i] + sufixlink;
-                await page.goto(fullLink, { timeout: 2000 })                               
-                const data = await awaitStatistics();
-                eachfixture[`Jogo ${i+1}`]= data//{teamSearch:teamSearch, ...nextFixture, ...data}
-            } catch (err) {
-                console.error('Erro identificado: ', err)
-                continue
-            }
+const linkMatchs = await page.evaluate(() => {
+    // before: .event__match>a
+    const matchs = Array.from(document.querySelectorAll('.summary-results>.sportName>.event__match>a'));
+    const link = matchs.map((e) => {
+        return e.getAttribute('href');
+    })
+    return link;
+})
 
 
-        }
-        matchs['matchs'] = eachfixture;
 
-        await browser.close();
-        return matchs
+const awaitStatistics = async () => {
+
+    try {
+        await page.waitForSelector('.wcl-category_ITphf', { timeout: 10000 })
+        await page.waitForSelector('.participant__participantName>a')
+        const cornerKicks = await page.evaluate(() => {
+            const teams = Array.from(document.querySelectorAll('.participant__participantName>a'));
+            const wrapper = Array.from(document.querySelectorAll('.wcl-category_ITphf'));
+            const content = wrapper.map((e) => {
+                const contenItSelf = Array.from(e.querySelectorAll('strong'));
+
+                return contenItSelf.map(e => e.innerText)
+            });
+
+            // Organize content into an object
+            const newContent = content.reduce((acc, e) => {
+                acc[e[1]] = { home: e[0], away: e[2] };
+                return acc;
+            }, {})
+
+
+
+            return {
+                Teams: { Teamhome: teams[0].innerText, TeamAway: teams[1].innerText },
+                Content: newContent,
+            };
+
+        });
+
+        return cornerKicks
+    } catch (err) {
+        console.error('Erro ao buscar estatísticas: ' + err);
+        return { erro: 'partida sem dados' }
+    }
+
+}
+
+
+
+matchs['teamSearch'] = correctName;
+matchs['nextFixture'] = nextFixture.nextMatch;
+const eachfixture = {}
+for (i = 0; i <= 4; i++) {
+    try {
+        const fullLink = linkMatchs[i] + sufixlink;
+        await page.goto(fullLink, { timeout: 2000 })
+        const data = await awaitStatistics();
+        eachfixture[`Jogo ${i + 1}`] = data//{teamSearch:teamSearch, ...nextFixture, ...data}
+    } catch (err) {
+        console.error('Erro identificado: ', err)
+        continue
+    }
+
+
+}
+matchs['matchs'] = eachfixture;
+
+await browser.close();
+return matchs
 
     }
     catch (err) {
-        console.error('Erro geral: ', err)
-        if (browser) {
-            await browser.close();
-        }
+    console.error('Erro geral: ', err)
+    if (browser) {
+        await browser.close();
     }
+}
 
 }
 
